a1df4677c03b6d89e6069f15be058d4f3ef52dc2
Parth <frostgameing0@gmail.com>
2024-08-20 00:40:53 +0530

    added some functions, application still not usable

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from terminal_funcs import display_options, insert_link
 
+links = []
 
-while quit not in ('y', 'Y'):
-    pass
+while True:
+    display_options()
+    user_input = int(input("Enter your choice :"));
+    match user_input:
+        case 1:
+            insert_link(links)
+        case _:
+            print(f"Invalid Input : {user_input}, Please try again.");
--- terminal_funcs.py
+++ terminal_funcs.py
@@ -3,3 +3,12 @@ from downloader import DowloadYT
 def display_options():
     print("Welcome to Youtube audio/video downloader!")
     print("What would you like to do?")
+    print("(1) Enter a link inside the list.")
+    print("(2) Download all audios/videos in the list.")
+    print("(3) Enter a link and download a single audio/video.")
+    print("(4) Change download path.")
+    print("(5) Exit.")
+
+def insert_link(link_list : list):
+    link_list.append(input("Enter link :"))
+    return link_list
