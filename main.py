9d0daa2da973e70356f10a422e4dba0526f79f49
Parth <frostgameing0@gmail.com>
2024-08-21 23:30:40 +0530

    Dont use, non-tested code+ logic not completed.

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from terminal_funcs import display_options, insert_link
+from terminal_app import TerminalApp
 
 links = []
 
+tapp = TerminalApp();
+
 while True:
-    display_options()
+    tapp.display_options()
     user_input = int(input("Enter your choice :"));
     match user_input:
         case 1:
-            insert_link(links)
+            tapp.insert_link(links)
+        case 2:
+            video_confirmation = input("Enter (V)ideo , (A)udio :")
+            if video_confirmation not in ['y', 'Y', 'Yes', 'YES', 'yes', 'YeS', 'yEs', 'YEs', 'yES', 'yeS']:
+                video_confirmation = True
+            else:
+                video_confirmation = False
+            tapp.download_all_links(video_confirmation)
+        case 3:
+            video_confirmation = input("Enter (V)ideo , (A)udio :")
+            if video_confirmation not in ['y', 'Y', 'Yes', 'YES', 'yes', 'YeS', 'yEs', 'YEs', 'yES', 'yeS']:
+                video_confirmation = True
+            else:
+                video_confirmation = False
+            link = input("Enter link :")
+            tapp.download(link, video_confirmation)
         case _:
             print(f"Invalid Input : {user_input}, Please try again.");
